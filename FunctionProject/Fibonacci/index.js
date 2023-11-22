28ff5a6ae9c798a68765a3f754d43aaacf9b7eb2
julia <julia.mejia@mail.escuelaing.edu.co>
2023-11-22 17:48:30 -0500

    Así sirvió

--- FunctionProject/Fibonacci/index.js
+++ FunctionProject/Fibonacci/index.js
@@ -1,23 +1,25 @@
-var bigInt = require("big-integer");
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     let nth = req.body.nth
-    let nth_1 = bigInt.one;
-    let nth_2 = bigInt.zero;
-    let answer = bigInt.zero;
 
-    if (nth < 0)
-        throw 'must be greater than 0'
-    else if (nth === 0)
-        answer = nth_2
-    else if (nth === 1)
-        answer = nth_1
-    else {
-        for (var i = 0; i < nth - 1; i++) {
-            answer = nth_2.add(nth_1)
-            nth_2 = nth_1
-            nth_1 = answer
+    const callFib = dynamicFibonacci();
+    answer = callFib(nth);
+
+
+    function dynamicFibonacci() {
+        let cache = {};
+
+        return function fib(n) {
+            if (n in cache) {
+                return cache[n];
+            } else if (n < 2) {
+                return n;
+            } else {
+                cache[n] = BigInt(fib(n - 1)) + BigInt(fib(n - 2));
+                return BigInt(cache[n]);
+            }
         }
     }
 
